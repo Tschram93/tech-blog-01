90edd25fca8f5bb6e2949b4d342a8d49a6cff072
TS93 <tschram93@yahoo.com>
2022-01-21 23:05:47 -0500

    server update: do you work?

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const path = require('path');
 const express = require('express');
+const routes = require('./controllers');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+
+const app = express();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create();
 
 const sess = {
-	secret: 'Secret',
+	secret: 'Super secret secret',
 	cookie: {},
 	resave: false,
 	saveUninitialized: true,
@@ -24,16 +24,15 @@ const sess = {
 
 app.use(session(sess));
 
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-	app.listen(PORT, () => console.log('Now listening'));
+	app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
