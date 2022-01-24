0448823439ec89d2dfea64640fe2fb68b0ee3382
TS93 <tschram93@yahoo.com>
2022-01-24 03:58:18 -0500

    syncing

---
+++ seeds/seed.js
@@ -0,0 +1,10 @@
+const seedUsers = require('./user-seeds');
+const seedPosts = require('./post-seeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+    
+}
+
+seedAll()
