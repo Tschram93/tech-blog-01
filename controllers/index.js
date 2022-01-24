957c6aad528a4159b18e07304412c8ba41411b87
TS93 <tschram93@yahoo.com>
2022-01-24 11:18:31 -0500

    seeding and launching server work; need to fix partials/handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const htmlRoutes = require('./html');
 
-router.use('/', homeRoutes);
+router.use('/', htmlRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
