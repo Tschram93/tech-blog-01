142cc57908994511a960d023b4185966e57e038b
TS93 <tschram93@yahoo.com>
2022-01-24 03:58:50 -0500

    seed finished

--- seeds/seed.js
+++ seeds/seed.js
@@ -4,7 +4,14 @@ const seedPosts = require('./post-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-    
-}
+	await sequelize.sync({ force: true });
+	console.log('\n----- DATABASE SYNCED ----- \n');
+	await seedUsers();
+	console.log('\n----- USERS SYNCED -----\n');
+	await seedPosts();
+	console.log('\n----- POSTS SYNCED -----\n');
 
-seedAll()
+	process.exit(0);
+};
+
+seedAll();
