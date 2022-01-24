221664620ec8d3e2c16a44a632d7e36a7cd77d15
TS93 <tschram93@yahoo.com>
2022-01-24 04:00:16 -0500

    rename variables to fit

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,5 @@
-const seedUsers = require('./user-seeds');
-const seedPosts = require('./post-seeds');
+const seedUsers = require('./userData');
+const seedPosts = require('./postData');
 
 const sequelize = require('../config/connection');
 
